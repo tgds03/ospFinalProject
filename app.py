9f565fb113e0799318c6adf80a00b8c084ccae91
kyw5205 <kyw5205@gmail.com>
2020-06-29 02:06:34 +0900

    modified

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 from flask import Flask
 from flask import render_template
 from flask import request
--- start.sh
+++ start.sh
@@ -3,15 +3,18 @@
 sudo apt-get install python3-pip
 sudo pip3 install flask
 sudo pip3 install numpy
-sudo pip3 install time
 sudo pip3 install requests
 sudo pip3 install bs4
 
-mkdir -p ospFinalProject/templates
-//tar -zxvf FILENAME.tar.gz -C /ospFinalProject     FILENAME에 파일 이름 추가하기
-cd ospFinalProject
-mv osp.html similar_analysis_pop.html word_analysis_pop.html ospFinalProject/templates
-sudo service elasticsearch start
-cd ../ospFinalProject
+mkdir -p _ospFinalProject/templates
+tar -zxvf temp.tar.gz -C _ospFinalProject
+cd _ospFinalProject/test
+mv osp.html similar_analysis_pop.html word_analysis_pop.html ../templates
+mv single_url_crawl.py app.py database.py ospMappings.json searchBody.json ../
+cd ..
+rm -rf test 
+cd ../elasticsearch-7.6.2
+./bin/elasticsearch -d
+cd ../_ospFinalProject
 sudo chmod 755 app.py
-python ./app.py
+./app.py
