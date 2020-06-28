92821770768e95917e1903d6007523ed8f840dac
tgds03 <xyzuct6800@naver.com>
2020-06-29 01:54:11 +0900

    debugging

--- start.sh
+++ start.sh
@@ -3,15 +3,13 @@
 sudo apt-get install python3-pip
 sudo pip3 install flask
 sudo pip3 install numpy
-sudo pip3 install time
 sudo pip3 install requests
 sudo pip3 install bs4
 
 mkdir -p ospFinalProject/templates
-//tar -zxvf FILENAME.tar.gz -C /ospFinalProject     FILENAME에 파일 이름 추가하기
-cd ospFinalProject
-mv osp.html similar_analysis_pop.html word_analysis_pop.html ospFinalProject/templates
+tar -zxvf ospFinal.tar.gz
+
 sudo service elasticsearch start
-cd ../ospFinalProject
+cd ospFinalProject
 sudo chmod 755 app.py
-python ./app.py
+./app.py
