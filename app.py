ac201ed1fd6749c2d9644c93eb1b94cb94b3b7ea
tgds03 <xyzuct6800@naver.com>
2020-06-28 19:35:54 +0900

    add feature) input with files

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def addurl():
 	url = request.form['homepage']
 	url = re.sub(r'\s+', '', url)
 	error = []
-	if url == None:
+	if url == '':
 		error.append('none')
 	elif url in urls:
 		error.append('duplicate')
@@ -40,7 +40,7 @@ def addurl():
 			doc.status = str(e)
 			doc.status = doc.status[0:doc.status.find(":")]
 		
-		#if url is accessible, print result
+		#if url is accessible, record result
 		if doc.status == None:
 			wordfreq = crawled['data']
 			doc.insert_words(wordfreq)
@@ -55,10 +55,43 @@ def addurl():
 
 	return render_template('osp.html', data=data, error=error)
 
+@app.route('/addurls', methods=['POST'])
+def addurls():
+	new_urls = request.form['urls'].split(',')
+	for url in new_urls:
+		url = re.sub(r'\s+', '', url)
+		if url == '' or url in urls: continue
+
+		doc = db.Document(url)
+		urls.append(url)
+		documents.append(doc)
+
+		#record url status
+		try:
+			crawled = crawls.single_url_crawl(url)
+		except Exception as e:
+			doc.status = str(e)
+			doc.status = doc.status[0:doc.status.find(":")]
+		
+		#if url is accessible, record result
+		if doc.status == None:
+			wordfreq = crawled['data']
+			doc.insert_words(wordfreq)
+			doc.record_time(crawled['time'])
+
+			total_es.insert_words(wordfreq)
+			total_es.total_info['url_list'].append(doc.url)
+			total_es.insert_document(doc)
+			total_es.update_total()
+	
+	#print recorded until now
+	data = [{"url":d.url, "time":d.crawl_time, "count":d.word_count, "status":d.status} for d in documents]
+	return render_template('osp.html', data=data)
+
 # @app.route('/similar', method=['POST'])
 # def print_similar():
 
 # @app.route('/analysis', method=['POST'])
 # def print_analysis():
 
-if __name__=="__main__":	app.run(debug=True)
+if __name__=="__main__": app.run(debug=True)
